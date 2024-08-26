852d1f6d2c7dfb20671fc1a1b4714f2b8fdae942
neerajnaik9 <neerajdnaik@gmail.com>
2024-08-26 23:02:42 +0530

    Add files via upload

---
+++ Modules/java/Assignment-1/assignment1.java
@@ -0,0 +1,131 @@
+class assignment1 { 
+
+public static void main(String[] args) 
+{
+/*
+//-----number is positive-------------
+int n=5;
+
+
+if(n>0)
+{
+System.out.println("number is positive");
+}
+else
+{
+System.out.println("number is negative");
+}
+
+}
+
+//-------------number is negative-----------------
+
+int n=-5;
+
+
+if(n<0)
+{
+System.out.println("number is negative");
+}
+else
+{
+System.out.println("number is positive");
+}
+
+}
+}
+
+
+// --------------program to find odd or even ----
+
+int n=5;
+
+
+if(n%2==0)
+{
+System.out.println("number is even");
+}
+else
+{
+System.out.println("number is odd");
+}
+
+}
+}
+
+//---------------good morning----------------
+
+double time=7.30;
+
+
+if(time>=5 && time<=12)
+{
+System.out.println("good morning");
+}
+
+}
+
+//-------------area of square----------
+int sidelength=3;
+int area=sidelength*sidelength;
+
+System.out.println("area of the square:" +area);
+
+
+}
+
+}
+
+//------------area of the rectangle------
+
+int length=3;
+int breadth=5;
+int area=length*breadth;
+
+System.out.println("area of the rectangle:" +area);
+
+
+}
+}
+*/
+//------Largest of three numbers-------
+int n1=5;
+int n2=6;
+int n3=7;
+
+
+if(n1>n2)
+{
+	if(n1>n3)
+	{
+	System.out.println("n1 is largest");
+
+	}
+
+	else
+	{
+	System.out.println("n3 is largest");
+	}
+}
+
+else
+{
+	if(n2>n3)
+	{
+	System.out.println("n2 is largest");
+
+	}
+
+	else
+	{
+	System.out.println("n3 is largest");
+	}
+}
+
+
+
+
+}
+
+}
+
